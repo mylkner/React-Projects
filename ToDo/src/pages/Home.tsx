bb7027eba7915d06b15606283d4101fac63cb0a8
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-06-08 15:43:39 +0100

    styles + error on attempt to add empty todo

--- ToDo/src/pages/Home.tsx
+++ ToDo/src/pages/Home.tsx
@@ -9,12 +9,14 @@ interface ToDo {
 const Home = () => {
     const [toAdd, setToAdd] = useState<string>("");
     const [todos, setTodos] = useState<ToDo[]>([]);
-
-    todos.forEach((element) => {
-        console.log(element.todo);
-    });
+    const [error, setError] = useState<string>("");
 
     const handleClick = (): void => {
+        setError("");
+        if (toAdd.trim() === "") {
+            setError("Input cannot be empty.");
+            return;
+        }
         setToAdd("");
         setTodos([...todos, { id: Date.now(), todo: toAdd }]);
     };
@@ -25,22 +27,43 @@ const Home = () => {
     };
 
     return (
-        <div>
-            <input
-                type="text"
-                value={toAdd}
-                onChange={(e) => setToAdd(e.target.value)}
-            />
-            <button onClick={handleClick}>Add</button>
-            {todos.map(({ id, todo }) => (
-                <ToDo
-                    key={id}
-                    id={id}
-                    todo={todo}
-                    handleDelete={handleDelete}
-                />
-            ))}
-        </div>
+        <>
+            <div className="flex gap-3 justify-center items-start p-5 mt-20">
+                <div className="flex flex-col gap-1 relative">
+                    <input
+                        type="text"
+                        value={toAdd}
+                        onChange={(e) => setToAdd(e.target.value)}
+                        className="bg-white rounded-md p-3"
+                        placeholder="todo..."
+                    />
+                    {error && (
+                        <p className="text-red-600 font-semibold text-left absolute top-full">
+                            {error}
+                        </p>
+                    )}
+                </div>
+
+                <button
+                    onClick={handleClick}
+                    className="bg-amber-900 p-3 text-white rounded-md active:bg-amber-950 cursor-pointer"
+                >
+                    Add
+                </button>
+            </div>
+
+            <div className="flex flex-col w-full gap-2 justify-center items-center p-5">
+                {todos.map(({ id, todo }, index) => (
+                    <ToDo
+                        key={id}
+                        index={index}
+                        id={id}
+                        todo={todo}
+                        handleDelete={handleDelete}
+                    />
+                ))}
+            </div>
+        </>
     );
 };
 
