c05d8becd64db0af67354d5ddbb9d7a1c9880173
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-06-08 13:53:40 +0100

    delete function

--- ToDo/src/components/ToDo.tsx
+++ ToDo/src/components/ToDo.tsx
@@ -1,10 +1,16 @@
 interface ToDoProps {
     id: number;
     todo: string;
+    handleDelete: (idToRemove: number) => void;
 }
 
-const ToDo = ({ id, todo }: ToDoProps) => {
-    return <div key={id}>{todo}</div>;
+const ToDo = ({ id, todo, handleDelete }: ToDoProps) => {
+    return (
+        <div key={id}>
+            {todo}
+            <button onClick={() => handleDelete(id)}>Delete</button>
+        </div>
+    );
 };
 
 export default ToDo;
--- ToDo/src/pages/Home.tsx
+++ ToDo/src/pages/Home.tsx
@@ -10,8 +10,18 @@ const Home = () => {
     const [toAdd, setToAdd] = useState<string>("");
     const [todos, setTodos] = useState<ToDo[]>([]);
 
+    todos.forEach((element) => {
+        console.log(element.todo);
+    });
+
     const handleClick = (): void => {
+        setToAdd("");
         setTodos([...todos, { id: Date.now(), todo: toAdd }]);
+    };
+
+    const handleDelete = (idToRemove: number): void => {
+        const filteredTodos = todos.filter(({ id }) => id !== idToRemove);
+        setTodos(filteredTodos);
     };
 
     return (
@@ -23,7 +33,12 @@ const Home = () => {
             />
             <button onClick={handleClick}>Add</button>
             {todos.map(({ id, todo }) => (
-                <ToDo key={id} id={id} todo={todo} />
+                <ToDo
+                    key={id}
+                    id={id}
+                    todo={todo}
+                    handleDelete={handleDelete}
+                />
             ))}
         </div>
     );
