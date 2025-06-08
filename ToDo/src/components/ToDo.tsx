0672bc8f8be048eb28ad1e21c14c415dd354448f
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-06-08 15:40:37 +0100

    styles

--- ToDo/src/components/ToDo.tsx
+++ ToDo/src/components/ToDo.tsx
@@ -1,14 +1,25 @@
 interface ToDoProps {
+    index: number;
     id: number;
     todo: string;
     handleDelete: (idToRemove: number) => void;
 }
 
-const ToDo = ({ id, todo, handleDelete }: ToDoProps) => {
+const ToDo = ({ index, id, todo, handleDelete }: ToDoProps) => {
     return (
-        <div key={id}>
-            {todo}
-            <button onClick={() => handleDelete(id)}>Delete</button>
+        <div
+            key={id}
+            className="flex gap-2 justify-between items-center bg-amber-600 max-w-[600px] w-full p-5 text-white text-lg rounded-xl"
+        >
+            <p>
+                {index + 1}. {todo}
+            </p>
+            <button
+                className="bg-amber-900 px-5 py-2 text-white rounded-md active:bg-amber-950 cursor-pointer"
+                onClick={() => handleDelete(id)}
+            >
+                Delete
+            </button>
         </div>
     );
 };
