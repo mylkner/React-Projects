e03c144f98c390c02951023abbf363bcf93e857d
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-07 20:48:17 +0100

    sample data

---
+++ WutheringWavesRotationArchive/src/Data/rotations.ts
@@ -0,0 +1,49 @@
+interface Rotations {
+    [character: string]: Rotation[];
+}
+
+export interface Rotation {
+    team: string;
+    rotation: string;
+}
+
+const rotations: Rotations = {
+    "Rover-Havoc": [
+        {
+            team: "",
+            rotation: "",
+        },
+    ],
+    Encore: [
+        {
+            team: "",
+            rotation: "",
+        },
+    ],
+    Camellya: [
+        {
+            team: "",
+            rotation: "",
+        },
+    ],
+    Jiyan: [
+        {
+            team: "",
+            rotation: "",
+        },
+    ],
+    Carlotta: [
+        {
+            team: "",
+            rotation: "",
+        },
+    ],
+    Changli: [
+        {
+            team: "",
+            rotation: "",
+        },
+    ],
+};
+
+export default rotations;
