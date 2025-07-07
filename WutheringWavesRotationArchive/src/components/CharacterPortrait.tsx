63c821b5da544a56b0544ceb22986e93b49778d6
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-07 23:53:27 +0100

    finish character portrait

--- WutheringWavesRotationArchive/src/components/CharacterPortrait.tsx
+++ WutheringWavesRotationArchive/src/components/CharacterPortrait.tsx
@@ -1,23 +1,31 @@
 import { Link } from "react-router";
-import type { PortraitData } from "../Data/portraitData";
+import type { CharacterData } from "../Data/characterData";
 
 interface CharacterSelectIconProps {
-    portraitData: PortraitData;
+    portraitData: CharacterData;
 }
 
 const CharacterSelectIcon = ({ portraitData }: CharacterSelectIconProps) => {
-    const { name, rarity, icon } = portraitData;
+    const { name, rarity, icons } = portraitData;
     const bgColor: string =
         rarity === "5 Star" ? "bg-yellow-600" : "bg-violet-500";
 
     return (
         <Link
             to={`/${name.toLowerCase()}`}
-            className={`flex flex-col ${bgColor} rounded-lg hover:cursor-pointer`}
+            className={`flex flex-col ${bgColor} rounded-lg hover:cursor-pointer relative`}
         >
             <img
+                src={icons.element}
+                className="absolute top-0 left-0 h-[40px] w-[40px]"
+            />
+            <img
+                src={icons.weapon}
+                className="absolute top-0 right-0 h-[40px] w-[40px]"
+            />
+            <img
                 className="w-full h-full object-fit-cover"
-                src={icon}
+                src={icons.character}
                 alt={name}
             />
             <p className="bg-neutral-300 text-center p-2 rounded-b-lg font-semibold">
