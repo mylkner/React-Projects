7b0b676400699148e13a8d4709baebe44a6c5d80
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-07 21:39:46 +0100

    added icons

--- WutheringWavesRotationArchive/src/Data/rotations.ts
+++ WutheringWavesRotationArchive/src/Data/rotations.ts
@@ -1,11 +1,16 @@
+const imgPathMaker = (name: string): string =>
+    `src/Assets/Resonator_${name}.webp`;
+
 export interface CharacterData {
     name: string;
+    icon: string;
     data: { team: string; rotation: string }[];
 }
 
 const db: CharacterData[] = [
     {
         name: "rover-havoc",
+        icon: imgPathMaker("Female_Rover"),
         data: [
             {
                 team: "",
@@ -15,6 +20,7 @@ const db: CharacterData[] = [
     },
     {
         name: "encore",
+        icon: imgPathMaker("Encore"),
         data: [
             {
                 team: "",
@@ -24,6 +30,7 @@ const db: CharacterData[] = [
     },
     {
         name: "camellya",
+        icon: imgPathMaker("Camellya"),
         data: [
             {
                 team: "",
@@ -33,6 +40,7 @@ const db: CharacterData[] = [
     },
     {
         name: "jiyan",
+        icon: imgPathMaker("Jiyan"),
         data: [
             {
                 team: "",
@@ -42,6 +50,7 @@ const db: CharacterData[] = [
     },
     {
         name: "carlotta",
+        icon: imgPathMaker("Carlotta"),
         data: [
             {
                 team: "",
@@ -51,6 +60,7 @@ const db: CharacterData[] = [
     },
     {
         name: "changli",
+        icon: imgPathMaker("Changli"),
         data: [
             {
                 team: "",
