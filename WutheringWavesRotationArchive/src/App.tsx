b4a554550264617c1489561bbbce1d997c2087d6
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-07 20:42:33 +0100

    added rotation page route

--- WutheringWavesRotationArchive/src/App.tsx
+++ WutheringWavesRotationArchive/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Header from "./components/Header";
 import CharacterList from "./pages/CharacterList";
 import NotFound from "./pages/NotFound";
+import RotationPage from "./pages/RotationPage";
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Header />
             <Routes>
                 <Route path="/" element={<CharacterList />} />
+                <Route path="/:character" element={<RotationPage />} />
                 <Route path="/*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
