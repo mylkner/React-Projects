dca36ac8b477d6376d7957cb3fe7809de1517281
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-07 20:11:05 +0100

    error component

--- WutheringWavesRotationArchive/src/App.tsx
+++ WutheringWavesRotationArchive/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
             <Header />
             <Routes>
                 <Route path="/" element={<CharacterList />} />
+                <Route path="/*" element={<NotFound />} />
             </Routes>
         </BrowserRouter>
     );
