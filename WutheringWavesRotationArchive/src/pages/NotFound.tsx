c82d8f6806b2059f5564afd9ea514cd91de68caf
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-07 23:55:33 +0100

    finish not found page

--- WutheringWavesRotationArchive/src/pages/NotFound.tsx
+++ WutheringWavesRotationArchive/src/pages/NotFound.tsx
@@ -1,13 +1,10 @@
-import { Link } from "react-router";
-
 const NotFound = () => {
     return (
-        <div className="w-full h-[95vh] flex flex-col items-center justify-center">
-            <h1>Oops!</h1>
-            <p>The character you are looking for could not be found.</p>
-            <Link to={"/"} className="errorLink">
-                Back to character list
-            </Link>
+        <div className="w-full h-[95vh] text-white flex flex-col items-center justify-center">
+            <h1 className="mb-5 text-4xl font-bold">Oops!</h1>
+            <p className="text-2xl">
+                The character you are looking for could not be found.
+            </p>
         </div>
     );
 };
