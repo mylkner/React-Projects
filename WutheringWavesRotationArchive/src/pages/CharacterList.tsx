3882104d6a4c6dacc26df9cf232d1f61e5e413a4
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-08 02:14:18 +0100

    ui for filters and adding them but no actual functionality

--- WutheringWavesRotationArchive/src/pages/CharacterList.tsx
+++ WutheringWavesRotationArchive/src/pages/CharacterList.tsx
@@ -1,14 +1,69 @@
+import { useState } from "react";
+import { IoSearch } from "react-icons/io5";
 import CharacterSelectIcon from "../components/CharacterPortrait";
-import portraitDb from "../Data/portraitData";
+import characterDb from "../Data/characterData";
+import { Rarities, Elements, Weapons } from "../types/characterDataTypes";
+import {
+    elementImgPathMaker,
+    rarityImgPathMaker,
+    weaponImgPathMaker,
+} from "../Helpers/imgPathMakers";
+import FilterGroup from "../components/FilterGroup";
 
 const CharacterList = () => {
+    const [search, setSearch] = useState<string>("");
+    const [selectedRarities, setSelectedRarities] = useState<Rarities[]>([]);
+    const [selectedElements, setSelectedElements] = useState<Elements[]>([]);
+    const [selectedWeapons, setSelectedWeapons] = useState<Weapons[]>([]);
+
+    const searchBar = (
+        <div className="flex items-center border w-fit py-2 px-4 gap-3 border-white rounded">
+            <IoSearch className="text-white text-2xl" />
+            <input
+                className="caret-white text-white outline-none placeholder:italic"
+                placeholder="Search..."
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+            />
+        </div>
+    );
+
     return (
-        <div className="flex flex-wrap gap-3 p-10">
-            {portraitDb
-                .sort((a, b) => a.name.localeCompare(b.name))
-                .map((data) => (
-                    <CharacterSelectIcon portraitData={data} />
-                ))}
+        <div className="flex flex-col flex-wrap gap-3 p-10">
+            <h1 className="text-white text-4xl">Characters</h1>
+            <hr className="text-white" />
+            <div className="flex flex-col lg:flex-row gap-5">
+                {searchBar}
+                <FilterGroup
+                    values={Object.values(Rarities)}
+                    selected={selectedRarities}
+                    setSelected={setSelectedRarities}
+                    imgPathMaker={rarityImgPathMaker}
+                />
+                <FilterGroup
+                    values={Object.values(Elements)}
+                    selected={selectedElements}
+                    setSelected={setSelectedElements}
+                    imgPathMaker={elementImgPathMaker}
+                />
+                <FilterGroup
+                    values={Object.values(Weapons)}
+                    selected={selectedWeapons}
+                    setSelected={setSelectedWeapons}
+                    imgPathMaker={weaponImgPathMaker}
+                />
+            </div>
+
+            <div className="flex flex-wrap gap-3">
+                {characterDb
+                    .sort((a, b) => a.name.localeCompare(b.name))
+                    .map((data) => (
+                        <CharacterSelectIcon
+                            key={data.name}
+                            portraitData={data}
+                        />
+                    ))}
+            </div>
         </div>
     );
 };
