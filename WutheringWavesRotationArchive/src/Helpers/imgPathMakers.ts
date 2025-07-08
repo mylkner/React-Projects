0f7b65ed4e43922b3ca21ce6fc9ea528a863172f
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-08 02:14:28 +0100

    rarityImgPathMaker template

--- WutheringWavesRotationArchive/src/Helpers/imgPathMakers.ts
+++ WutheringWavesRotationArchive/src/Helpers/imgPathMakers.ts
@@ -2,6 +2,7 @@ import { Elements, Weapons } from "../types/characterDataTypes";
 
 export const resonatorImgPathMaker = (name: string): string =>
     `src/Assets/Resonators/Resonator_${name}.webp`;
+export const rarityImgPathMaker = (name: string): string => `${name}`;
 export const elementImgPathMaker = (name: Elements): string =>
     `src/Assets/Elements/${name}.webp`;
 export const weaponImgPathMaker = (name: Weapons): string =>
