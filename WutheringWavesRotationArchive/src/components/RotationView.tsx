2e01c4759b541555bddd1de96943729bcca055ba
= <=>
2025-07-08 14:52:26 +0100

    finish rotation view

---
+++ WutheringWavesRotationArchive/src/components/RotationView.tsx
@@ -0,0 +1,32 @@
+import characterDb, { type CharacterData } from "../Data/characterData";
+import CharacterPortrait from "./CharacterPortrait";
+
+interface RotationViewProps {
+    team: string[];
+    rotation: string;
+    index: number;
+}
+
+const RotationView = ({ team, rotation, index }: RotationViewProps) => {
+    const characterMap: Map<string, CharacterData> = new Map(
+        characterDb.map((char) => [char.name, char])
+    );
+    const characterPortraits: CharacterData[] = team
+        .map((charName) => characterMap.get(charName))
+        .filter((char): char is CharacterData => char !== undefined);
+
+    return (
+        <div className="flex flex-col gap-3 mb-20">
+            <h1 className="text-white text-2xl">{index + 1}</h1>
+            <hr className="text-white" />
+            <div className="flex flex-row gap-3">
+                {characterPortraits.map((char) => (
+                    <CharacterPortrait key={char.name} portraitData={char} />
+                ))}
+            </div>
+            <p className="text-white text-xl">{rotation}</p>
+        </div>
+    );
+};
+
+export default RotationView;
