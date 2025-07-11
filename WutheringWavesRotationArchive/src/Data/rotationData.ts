fe408aa9692713415e46d1c04bdfc567533f8f15
Mylo Faulkner <mylofaulkner27@gmail.com>
2025-07-11 20:28:38 +0100

    have to change lupa rotations

--- WutheringWavesRotationArchive/src/Data/rotationData.ts
+++ WutheringWavesRotationArchive/src/Data/rotationData.ts
@@ -3,7 +3,7 @@ export interface RotationData {
     data: { team: string[]; rotation: string }[];
 }
 
-//todo encore, lupa, changli
+//todo all lupa rotations
 
 const rotationDb: RotationData[] = [
     {
@@ -32,21 +32,7 @@ const rotationDb: RotationData[] = [
             {
                 team: ["Encore", "Sanhua", "Verina"],
                 rotation:
-                    "Encore E, *Intro Sanhua E-R-Forte, Encore E, Verina BA3-E-Q-R, Encore Q, Verina Forte, Intro Sanhua Forte-Q, Encore R-BA4-E-BA4-BA4-E-E. *Second rotation onwards.",
-            },
-            {
-                team: ["Encore", "Lupa", "Shorekeeper"],
-                rotation:
-                    "Shorekeeper (BA4-HA/Intro)-E, Lupa E-E, Shorekeeper BA4-HA-Q-R, Intro Lupa Q-R-E-MA3-BA, Encore Q, Lupa E, Intro Encore R-BA4-E-BA4-BA4-E-HA.",
-            },
-            {
-                team: ["Encore", "Lupa", "Verina"],
-                rotation:
-                    "*Intro Lupa E-E, Verina BA3-E-Q-R-Forte, Intro Lupa Q-R-E-MA3-BA, Encore Q, Lupa E, Intro Encore R-BA4-E-BA4-BA4-E-HA. *Second rotation onwards.",
-            },
-            {
-                team: ["Encore", "Lupa", "Changli"],
-                rotation: "3",
+                    "Encore E, *Intro Sanhua E-R-Forte, Encore E, Verina BA3-E-Q-R, Encore Q, Verina Forte, Intro Sanhua Forte-Q, Encore R-BA4-E-BA4-BA4-E. *Second rotation onwards.",
             },
         ],
     },
@@ -62,16 +48,6 @@ const rotationDb: RotationData[] = [
                 team: ["Camellya", "Sanhua", "Verina"],
                 rotation:
                     "*Intro Sanhua E-R-Forte, Camellya Q, Verina BA3-E-Q-R-Forte, Intro Sanhua Forte-Q, Intro Camellya E-R-HA-Jump-Forte-E-HA-E. *Second rotation onwards.",
-            },
-            {
-                team: ["Camellya", "Lupa", "Shorekeeper"],
-                rotation:
-                    "Shorekeeper (BA4-HA/Intro)-E, Lupa E-E, Shorekeeper BA4-HA-Q-R, Intro Lupa Q-R-E-MA3-BA, Camellya Q-MA, Lupa E, Intro Camellya E-R-HA-Jump-Forte-E-HA-E.",
-            },
-            {
-                team: ["Camellya", "Lupa", "Verina"],
-                rotation:
-                    "*Intro Lupa E-E, Verina BA3-E-Q-R-Forte, Intro Lupa Q-R-E-MA3-BA, Camellya Q-MA, Lupa E, Intro Camellya E-R-HA-Jump-Forte-E-HA-E. *Second rotation onwards.",
             },
         ],
     },
@@ -106,32 +82,18 @@ const rotationDb: RotationData[] = [
             {
                 team: ["Carlotta", "Changli", "Shorekeeper"],
                 rotation:
-                    "Carlotta HA-E-E, Changli R-Forte, *Shorekeeper (BA4-HA/Intro)-E, Changli E, Carlotta Q, Changli TS:Conquest, Shorekeeper BA4-HA-Q-R, Intro Changli TS:Charge-E-TS:Conquest, Carlotta HA-E-E-MA-Forte, Changli Q, Carlotta R6-HA-E-E, Intro Changli TS:Charge-MA-Forte-R-Forte. *Repeat from here.",
+                    "Carlotta HA-E-E, Changli *(Intro TS:Charge-MA-Forte-)R-Forte, *Shorekeeper (BA4-HA/Intro)-E, Changli E, Carlotta Q, Changli TS:Conquest, Shorekeeper BA4-HA-Q-R, Intro Changli TS:Charge-E-TS:Conquest, Carlotta HA-E-E-MA-Forte, Changli Q, Carlotta R6. *Second rotation onwards.",
             },
             {
                 team: ["Carlotta", "Changli", "Verina"],
                 rotation:
                     "*Intro Carlotta HA-E-E, Changli R-Forte, Verina BA3-E-Q-R, Changli E, Carlotta Q, Changli TS:Charge-Q, Verina Forte, Intro Changli TS:Charge-E-TS:Conquest, Carlotta HA-E-E-MA-Forte-R6, Intro Changli TS:Charge-MA-Forte. *Second rotation onwards.",
             },
-            {
-                team: ["Carlotta", "Lupa", "Shorekeeper"],
-                rotation:
-                    "Carlotta HA-E-E, Shorekeeper (BA4-HA/Intro)-E, Lupa E-E, Carlotta MA-Q, Shorekeeper BA4-HA-Q-R, Intro Lupa Q-R-E-MA3-BA, Carlotta HA-E-E, Lupa E, Intro Carlotta MA-Forte-R6.",
-            },
-            {
-                team: ["Carlotta", "Lupa", "Verina"],
-                rotation:
-                    "Carlotta HA-E-E, *Intro Lupa E-E, Carlotta MA-Q, Verina BA3-E-Q-R-Forte, Intro Lupa Q-R-E-MA3-BA, Carlotta HA-E-E, Lupa E, Intro Carlotta MA-Forte-R6. *Second rotation onwards.",
-            },
         ],
     },
     {
         name: "Changli",
         data: [
-            {
-                team: ["Encore", "Lupa", "Changli"],
-                rotation: "3",
-            },
             {
                 team: ["Carlotta", "Changli", "Shorekeeper"],
                 rotation:
@@ -149,13 +111,11 @@ const rotationDb: RotationData[] = [
         data: [
             {
                 team: ["Encore", "Lupa", "Shorekeeper"],
-                rotation:
-                    "Shorekeeper (BA4-HA/Intro)-E, Lupa E-E, Shorekeeper BA4-HA-Q-R, Intro Lupa Q-R-E-MA3-BA, Encore Q, Lupa E, Intro Encore R-BA4-E-BA4-BA4-E-HA.",
+                rotation: "To be added",
             },
             {
                 team: ["Encore", "Lupa", "Verina"],
-                rotation:
-                    "*Intro Lupa E-E, Verina BA3-E-Q-R-Forte, Intro Lupa Q-R-E-MA3-BA, Encore Q, Lupa E, Intro Encore R-BA4-E-BA4-BA4-E-HA. *Second rotation onwards.",
+                rotation: "2",
             },
             {
                 team: ["Encore", "Lupa", "Changli"],
@@ -163,23 +123,19 @@ const rotationDb: RotationData[] = [
             },
             {
                 team: ["Camellya", "Lupa", "Shorekeeper"],
-                rotation:
-                    "Shorekeeper (BA4-HA/Intro)-E, Lupa E-E, Shorekeeper BA4-HA-Q-R, Intro Lupa Q-R-E-MA3-BA, Camellya Q-MA, Lupa E, Intro Camellya E-R-HA-Jump-Forte-E-HA-E.",
+                rotation: "4",
             },
             {
                 team: ["Camellya", "Lupa", "Verina"],
-                rotation:
-                    "*Intro Lupa E-E, Verina BA3-E-Q-R-Forte, Intro Lupa Q-R-E-MA3-BA, Camellya Q-MA, Lupa E, Intro Camellya E-R-HA-Jump-Forte-E-HA-E. Second rotation onwards.",
+                rotation: "5",
             },
             {
                 team: ["Carlotta", "Lupa", "Shorekeeper"],
-                rotation:
-                    "Carlotta HA-E-E, Shorekeeper (BA4-HA/Intro)-E, Lupa E-E, Carlotta MA-Q, Shorekeeper BA4-HA-Q-R, Intro Lupa Q-R-E-MA3-BA, Carlotta HA-E-E, Lupa E, Intro Carlotta MA-Forte-R6.",
+                rotation: "6",
             },
             {
                 team: ["Carlotta", "Lupa", "Verina"],
-                rotation:
-                    "Carlotta HA-E-E, *Intro Lupa E-E, Carlotta MA-Q, Verina BA3-E-Q-R-Forte, Intro Lupa Q-R-E-MA3-BA, Carlotta HA-E-E, Lupa E, Intro Carlotta MA-Forte-R6. *Second rotation onwards.",
+                rotation: "7",
             },
         ],
     },
